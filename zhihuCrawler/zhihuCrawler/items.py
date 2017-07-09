49b7c5ff012a5fc32255988229567e4d1149f886
wk633 <wk633@outlook.com>
2017-07-08 20:27:02 -0700

    add question and answer item

--- zhihuCrawler/zhihuCrawler/items.py
+++ zhihuCrawler/zhihuCrawler/items.py
@@ -12,3 +12,27 @@ class ZhihucrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ZhihuQuestionItem(scrapy.Item):
+    zhihu_id = scrapy.Field()
+    topics = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    answer_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    watch_user_num = scrapy.Field()
+    click_num = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+class ZhihuAnswerItem(scrapy.Item):
+    zhihu_id = scrapy.Field()
+    url = scrapy.Field()
+    question_id = scrapy.Field()
+    author_id = scrapy.Field()
+    content = scrapy.Field()
+    praise_num = scrapy.Field()
+    comments_num = scrapy.Field()
+    crawl_time = scrapy.Field()
+    create_time = scrapy.Field()
+    update_time = scrapy.Field()
